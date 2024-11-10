1a1a6692f1fe52073c85e7d97817035eb6c4a4b6
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-11-09 20:00:51 -0500

    Added some stuff for connect a database

--- src/main/java/ec/edu/espol/proyectoed/App.java
+++ src/main/java/ec/edu/espol/proyectoed/App.java
@@ -1,5 +1,7 @@
 package ec.edu.espol.proyectoed;
 
+import ec.edu.espol.proyectoed.model.Contact;
+import ec.edu.espol.proyectoed.model.FirebaseDatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -20,6 +22,26 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        /*
+            //Add user___________________________________________
+                // Crear una instancia de Usuario
+                Contact user = new Contact("Matías", "Collaguazo", "0987654321", "matiascoll07@hotmail.com");
+
+                // Obtener la instancia única de FirebaseDatabase y guardar el usuario
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                db.saveData("users", user.toJson());
+
+                System.out.println("Usuario guardado en la base de datos.");
+            //_____________________________________________
+        */
+        
+        /*Delete user
+            FirebaseDatabase db = FirebaseDatabase.getInstance();
+            db.deleteData("users");
+        
+        
+        */
+        
     }
 
     static void setRoot(String fxml) throws IOException {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ec.edu.espol.proyectoed {
     requires javafx.fxml;
 
     opens ec.edu.espol.proyectoed to javafx.fxml;
+    opens ec.edu.espol.proyectoed.model to com.google.gson;
     exports ec.edu.espol.proyectoed;
     requires com.google.gson;
 }
