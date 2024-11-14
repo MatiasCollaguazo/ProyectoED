74b6dff1f007f580bb43266779289331f4e690c2
Leonel Cabrera <leoancab@espol.edu.ec>
2024-11-14 17:25:28 -0500

    Creacion objeto Contacto

---
+++ src/main/java/ec/edu/espol/proyectoed/Contacto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectoed;
+
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author lacab
+ */
+public class Contacto {
+    private String nombre;
+    private String numeroTlf;
+    private Image foto;
+    private String direccionCasa;
+    private String direccionTrabajo;
+    private String emailPersonal;
+    private String trabajo;
+    private ArrayList<String> numerosTlfs;
+}
