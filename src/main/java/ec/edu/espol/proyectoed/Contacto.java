e9443cda81f7cb1a025e7067ecec254a5f645bd6
Leonel Cabrera <leoancab@espol.edu.ec>
2024-11-14 17:29:57 -0500

    Metodos getters y setters

--- src/main/java/ec/edu/espol/proyectoed/Contacto.java
+++ src/main/java/ec/edu/espol/proyectoed/Contacto.java
@@ -20,4 +20,83 @@ public class Contacto {
     private String emailPersonal;
     private String trabajo;
     private ArrayList<String> numerosTlfs;
+
+    public Contacto(String numeroTlf) {
+        this.numeroTlf = numeroTlf;
+    }
+
+    public Contacto(String nombre, String numeroTlf, Image foto, String direccionCasa, String direccionTrabajo, String emailPersonal, String trabajo, ArrayList<String> numerosTlfs) {
+        this.nombre = nombre;
+        this.numeroTlf = numeroTlf;
+        this.foto = foto;
+        this.direccionCasa = direccionCasa;
+        this.direccionTrabajo = direccionTrabajo;
+        this.emailPersonal = emailPersonal;
+        this.trabajo = trabajo;
+        this.numerosTlfs = numerosTlfs;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroTlf() {
+        return numeroTlf;
+    }
+
+    public void setNumeroTlf(String numeroTlf) {
+        this.numeroTlf = numeroTlf;
+    }
+
+    public Image getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Image foto) {
+        this.foto = foto;
+    }
+
+    public String getDireccionCasa() {
+        return direccionCasa;
+    }
+
+    public void setDireccionCasa(String direccionCasa) {
+        this.direccionCasa = direccionCasa;
+    }
+
+    public String getDireccionTrabajo() {
+        return direccionTrabajo;
+    }
+
+    public void setDireccionTrabajo(String direccionTrabajo) {
+        this.direccionTrabajo = direccionTrabajo;
+    }
+
+    public String getEmailPersonal() {
+        return emailPersonal;
+    }
+
+    public void setEmailPersonal(String emailPersonal) {
+        this.emailPersonal = emailPersonal;
+    }
+
+    public String getTrabajo() {
+        return trabajo;
+    }
+
+    public void setTrabajo(String trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    public ArrayList<String> getNumerosTlfs() {
+        return numerosTlfs;
+    }
+
+    public void setNumerosTlfs(ArrayList<String> numerosTlfs) {
+        this.numerosTlfs = numerosTlfs;
+    }
 }
